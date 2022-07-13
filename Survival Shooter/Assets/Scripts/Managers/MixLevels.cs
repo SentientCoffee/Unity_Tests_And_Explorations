837be0a7539542dbc712e89f0d0899f6f97b29d5
Daniel Presas <22991229+SentientCoffee@users.noreply.github.com>
2022-07-12 22:52:22 -0400

    [Add] Old Unity projects

---
+++ Survival Shooter/Assets/Scripts/Managers/MixLevels.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Audio;
+
+public class MixLevels : MonoBehaviour {
+	public AudioMixer masterMixer;
+
+	float sfxLvl = 0f;
+	float musicLvl = 0f;
+
+	public void SetSfxLvl(float sfxLvl) {
+		masterMixer.SetFloat ("sfxVol", sfxLvl);
+	}
+
+	public void SetMusicLvl(float musicLvl) {
+		masterMixer.SetFloat ("musicVol", musicLvl);
+	}
+
+	public void SetAudioToggle(bool test) {
+		if (test == true) {
+			SetSfxLvl (sfxLvl);
+			SetMusicLvl (musicLvl);
+		}
+		else {
+			SetSfxLvl (-80f);
+			SetMusicLvl (-80f);
+		}
+	}
+}
